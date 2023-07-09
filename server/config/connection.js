8febe29feb94eaeccdf9c4be52d4f7daa9d663d2
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2023-07-08 22:10:48 -0400

    mongodb connection for deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 
 // generate the database name with the end of the string
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mindflip');
+const connection = mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mindflip', {useNewUrlParser:true, useUnifiedTopology: true});
+
+connection
+    .then(db => {
+        console.log("Successfully connected to MongoDB");
+        return db;
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
 
 module.exports = mongoose.connection;
