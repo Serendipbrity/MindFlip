73f30ece23815290b58b80c8f0a0897c9c5f1c24
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2023-07-27 18:56:53 -0400

    styling

--- client/src/components/Categories/index.js
+++ client/src/components/Categories/index.js
@@ -11,38 +11,38 @@ const Categories = (props) => {
   const categories = dataViewCategories?.viewCategories || [];
   const { showCategories } = props;
 
-      // modal starts off not showing
-    const [modalIsOpen, setIsOpen] = useState(false);
-    const [currentCategory, setCurrentCategory] = useState(null);
+  // modal starts off not showing
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [currentCategory, setCurrentCategory] = useState(null);
   function openModal(category) {
     setCurrentCategory(category);
     setIsOpen(true);
   }
-    
+
   const [input, setInput] = useState("");
-    
+
   function closeModal() {
     setIsOpen(false);
   }
-    
-    // update category function
-    const [updateCategory] = useMutation(UPDATE_CATEGORY);
-    const handleUpdateCategory = async () => { 
-        try { 
-            await updateCategory({
-            variables: {
-                id: currentCategory._id,
-                category: input,
-            },
-            });
-            closeModal();
-            // alert user that category was updated and re load the page so that it shows
-            window.alert('Category Updated!').location.reload();
-        } catch (err) { 
-            console.error(err);
-        }
-     }
-    
+
+  // update category function
+  const [updateCategory] = useMutation(UPDATE_CATEGORY);
+  const handleUpdateCategory = async () => {
+    try {
+      await updateCategory({
+        variables: {
+          id: currentCategory._id,
+          category: input,
+        },
+      });
+      closeModal();
+      // alert user that category was updated and re load the page so that it shows
+      window.alert("Category Updated!").location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div>
       {/* if loading data */}
@@ -58,6 +58,7 @@ const Categories = (props) => {
             return (
               <div className="flashcards-container" key={category._id}>
                 <div className="scene">
+                  <div className="card">
                     <div className="card__face card__face--front">
                       {category.category}
                     </div>
@@ -69,6 +70,7 @@ const Categories = (props) => {
                     </div>
                   </div>
                 </div>
+              </div>
             );
           })}
         </div>
